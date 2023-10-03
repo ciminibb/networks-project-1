763d7700fcd1f6f8e631804f66cf5a9e27bda59c
Ben Cimini <ciminibb@mail.uc.edu>
2023-10-02 21:55:42 -0400

    part b checkpoint

--- web-server/WebServer.java
+++ web-server/WebServer.java
@@ -82,9 +82,108 @@ final class HttpRequest implements Runnable {
             System.out.println(headerLine);
         }
 
+        // Extract <fileName> from <requestLine>. Assume all are GET requests,
+        // meaning the method specification can be skipped.
+        StringTokenizer tokens = new StringTokenizer(requestLine);
+        tokens.nextToken();
+        String fileName = tokens.nextToken();
+
+        // Ensure file request is within the current directory by prepending "."
+        // for local path. The file is now prepared for manipulation.
+        fileName = "." + fileName;
+
+        // The first step of sending a file is opening it. A <FileInputStream>
+        // object is needed to do so. Note <FileNotFoundException> is caught in
+        // order to avoid terminating the thread.
+        FileInputStream fis = null;
+        boolean fileExists = true;
+
+        try {
+            fis = new FileInputStream(fileName);
+        }
+        catch (FileNotFoundException e) {
+            fileExists = false; // There is a special response for this.
+        }
+
+        // Construct response message using variables <statusLine>,
+        // <contentTypeLine>, and <entityBody>. They represent standard parts of
+        // an HTTP response.
+        String statusLine = null;
+        String contentTypeLine = null;
+        String entityBody = null;
+
+        if (fileExists) {
+            statusLine = "HTTP/1.0 200 OK" + CRLF; // Don't forget CRLF!
+            contentTypeLine = "Content-type: " + contentType(fileName) + CRLF;
+        }
+        else {
+            // Respond with "404 Not Found" if the requested file doesn't exist.
+            statusLine = "HTTP/1.0 404 Not Found" + CRLF;
+            contentTypeLine = "Content-type: text/html" + CRLF; // The response
+                                                                // content when
+                                                                // file doesn't
+                                                                // exist is
+                                                                // always HTML
+                                                                // text.
+            entityBody =
+                "<HTML>"
+                + "<HEAD><TITLE>Not Found</TITLE></HEAD>"
+                + "<BODY>Not Found</BODY></HTML>";
+        }
+
+        // Send <statusLine> and <contentTypeLine> to browser by writing to
+        // <os>.
+        os.writeBytes(statusLine);
+        os.writeBytes(contentTypeLine);
+        os.writeBytes(CRLF); // Signal end of header lines.
+
+        // Send <entityBody> to browser by the same means. What is sent is,
+        // again, contingent on whether the requested file exists.
+        if (fileExists) {
+            sendBytes(fis, os); // Send the file by <sendBytes> method.
+            fis.close();
+        }
+        else {
+            os.writeBytes(entityBody); // Send "404 Not Found."
+        }
+
         // Close streams and socket.
         os.close();
         br.close();
         socket.close();
+    }
+
+    // This method writes a file onto the socket's output stream, thereby
+    // writing to the requesting client.
+    private static void
+        sendBytes(FileInputStream fis, OutputStream os) throws Exception {
+        byte[] buffer = new byte[1024]; // <buffer> holds bytes on their way.
+        int bytes = 0;
+
+        // Copy requested file into the socket's output stream.
+        while((bytes = fis.read(buffer)) != -1 ) {
+            os.write(buffer, 0, bytes);
+        }
+    }
+
+    // This method determines a file's MIME type from its name.
+    private static String contentType(String fileName) {
+        // This server should handle HTML text.
+        if (fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+            return "text/html";
+        }
+
+        // This server should handle JPEG images.
+        if (fileName.endsWith(".jpeg") || fileName.endsWith(".jpg")) {
+            return "image/jpeg";
+        }
+
+        // This server should handle GIF images.
+        if (fileName.endsWith(".gif")) {
+            return "image/gif";
+        }
+
+        // Many other content types are ignored by this server.
+        return "application/octet-stream";
     }
 }
