e5212d80a36db523e002dc25f9854ba1eafad8f7
Ben Cimini <ciminibb@mail.uc.edu>
2023-10-05 20:23:23 -0400

    the ftp client works hahahahaha

---
+++ web-server/FtpTest.java
@@ -0,0 +1,15 @@
+public class FtpTest {
+    public static void main(String argv[]) {
+        // Create instance of <FtpClient>.
+        FtpClient myClient = new FtpClient();
+
+        // Connect to the server.
+        myClient.connect("ciminibb", "cs4065$$GIO");
+
+        // Retrieve file from the server.
+        myClient.getFile("ftp_test.txt");
+
+        // Disconnect from the server.
+        myClient.disconnect();
+    }
+}
